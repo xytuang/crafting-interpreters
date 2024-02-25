8da4833738a48d7a973de47e2f46c7479ec4729a
Xiang Yu Tuang <xiangyutuang@gmail.com>
2024-02-25 15:31:29 -0500

    Finished Chapter 5

--- jlox/GenerateAst.java
+++ jlox/GenerateAst.java
@@ -21,14 +21,27 @@ public class GenerateAst {
         writer.println();
         writer.println("abstract class " + baseName + " {");
 
+        defineVisitor(writer, baseName, types);
+
         for (String type : types) {
             String className = type.split(":")[0].trim();
             String fields = type.split(":")[1].trim();
             defineType(writer, baseName, className, fields);
             writer.println();
         }
+        writer.println("    abstract <R> R accept(Visitor<R> visitor);");
         writer.println("}");
         writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String> types) {
+        writer.println("    interface Visitor<R> {");
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase()+ ");");
+        }
+        writer.println("    }");
+        writer.println();
     }
     
     private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
@@ -41,6 +54,11 @@ public class GenerateAst {
             writer.println("                this." + name + " = " + name + ";");
         }
         writer.println("        }");
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }"); 
         writer.println();
         for (String field : fields) {
             writer.println("        final " + field + ";");
