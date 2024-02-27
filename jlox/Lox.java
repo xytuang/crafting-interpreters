df8067e07b3dd85f7232e8661a60f0eca2475b2c
Xiang Yu Tuang <xiangyutuang@gmail.com>
2024-02-26 19:57:45 -0500

    Finished chapter 7

--- jlox/Lox.java
+++ jlox/Lox.java
@@ -8,7 +8,9 @@ import java.util.List;
 // import java.util.Scanner;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -25,6 +27,7 @@ public class Lox {
         byte[] bytes = Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError) System.exit(65);
+        if(hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -49,11 +52,16 @@ public class Lox {
         Expr expression = parser.parse();
 
         if (hadError) return;
-        System.out.println(new AstPrinter().print(expression));
+        interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
         report(line, "", message);
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line +"]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
